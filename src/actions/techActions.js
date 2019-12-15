1ac31f37e19632ea88247a4ca7399c2ed2147517
Paul <paultowers2005@yahoo.com.au>
2019-12-15 22:46:34 +1100

    delete tech functionality

--- src/actions/techActions.js
+++ src/actions/techActions.js
@@ -54,6 +54,28 @@ export const addTech = tech => async dispatch => {
   }
 };
 
+// Delete Tech
+
+export const deleteTech = id => async dispatch => {
+  try {
+    setLoading();
+
+    const res = await fetch(`/techs/${id}`, {
+      method: "DELETE"
+    });
+
+    dispatch({
+      type: DELETE_TECH,
+      payload: id
+    });
+  } catch (error) {
+    dispatch({
+      type: TECHS_ERROR,
+      payload: error.response.statusText
+    });
+  }
+};
+
 // Set Loading To true
 
 export const setLoading = () => {
