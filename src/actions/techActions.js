e2b70d469f04ef72275e11d0087295d60a4fcb9f
Paul <paultowers2005@yahoo.com.au>
2019-12-15 21:50:36 +1100

    created add new tech functionality

--- src/actions/techActions.js
+++ src/actions/techActions.js
@@ -27,6 +27,33 @@ export const getTechs = () => async dispatch => {
   }
 };
 
+// Add technician to server
+
+export const addTech = tech => async dispatch => {
+  try {
+    setLoading();
+
+    const res = await fetch("/techs", {
+      method: "POST",
+      body: JSON.stringify(tech),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    });
+    const data = await res.json();
+
+    dispatch({
+      type: ADD_TECH,
+      payload: data
+    });
+  } catch (error) {
+    dispatch({
+      type: TECHS_ERROR,
+      payload: error.response.statusText
+    });
+  }
+};
+
 // Set Loading To true
 
 export const setLoading = () => {
