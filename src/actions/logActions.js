0377852333ca2382c1b61ded5bf8fbbe2b8ac54a
Paul <paultowers2005@yahoo.com.au>
2019-12-15 20:36:14 +1100

    added delete log functionality

--- src/actions/logActions.js
+++ src/actions/logActions.js
@@ -1,4 +1,10 @@
-import { GET_LOGS, SET_LOADING, LOGS_ERROR } from "./types";
+import {
+  GET_LOGS,
+  SET_LOADING,
+  LOGS_ERROR,
+  ADD_LOG,
+  DELETE_LOG
+} from "./types";
 
 //Get Logs From Server
 export const getLogs = () => async dispatch => {
@@ -11,6 +17,52 @@ export const getLogs = () => async dispatch => {
     dispatch({
       type: GET_LOGS,
       payload: data
+    });
+  } catch (error) {
+    dispatch({
+      type: LOGS_ERROR,
+      payload: error.response.data
+    });
+  }
+};
+
+// Add new log
+
+export const addLog = log => async dispatch => {
+  try {
+    setLoading();
+    const res = await fetch("/logs", {
+      method: "POST",
+      body: JSON.stringify(log),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    });
+    const data = await res.json();
+
+    dispatch({
+      type: ADD_LOG,
+      payload: data
+    });
+  } catch (error) {
+    dispatch({
+      type: LOGS_ERROR,
+      payload: error.response.data
+    });
+  }
+};
+
+// Delete Log from serveer
+
+export const deleteLog = id => async dispatch => {
+  try {
+    setLoading();
+
+    const res = await fetch(`/logs/${id}`, { method: "DELETE" });
+
+    dispatch({
+      type: DELETE_LOG,
+      payload: id
     });
   } catch (error) {
     dispatch({
